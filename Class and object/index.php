10c1340b734f48f73ebad258ea371922d7bc80a8
Yoga Pratama <63631251+Yogapratama1109@users.noreply.github.com>
2020-08-20 17:18:59 +0700

    Add files via upload

---
+++ Class and object/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Sintak class
+class Belajar{
+    public $buku;   // Property
+
+    // Method
+    public function sekolah(){
+
+    }
+}
+
+// Sintak object
+class BukuCerita{
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+
+    public function BeliBuku(){
+        return "Beli buku ayo dibeli";
+    }
+}
+
+?>
